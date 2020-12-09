64fe51070c0cb2a541627f613fbbe1e24370b983
cassymarie <cassy2205@gmail.com>
2020-12-09 16:12:02 -0700

    job update complete

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -7,17 +7,10 @@ class JobController < ApplicationController
         erb :'jobs/new'
     end
 
-    # get '/jobsite/:id/jobs/new/area' do
-    #     @jobsite = Jobsite.find(params[:id])
-    #     @jobs = @jobsite.jobs
-    #     @site_areas = @jobsite.site_areas
-    #     @job_edit = params[:job]
-    #     erb :'jobs/new'
-    # end
-
     get '/jobsite/:id/jobs/edit/:job_id' do
         @jobsite = Jobsite.find(params[:id])
         @job = Job.find(params[:job_id])
+        @jobs = @jobsite.jobs
         @site_areas = @jobsite.site_areas
         erb :'jobs/edit'
     end
@@ -39,17 +32,15 @@ class JobController < ApplicationController
         erb :'jobs/delete'
     end
 
-
-
     post '/jobsite/:id/jobs/new/area' do
-        new_area = Area.create(:code => params[:area][:code].upcase, :name => params[:area][:code].capitalize)
+        @job = Job.find(params[:job_id].to_i)
+        new_area = Area.create(:code => params[:area][:code].upcase, :name => params[:area][:name].capitalize)
+        @job.areas << new_area
         @jobsite = Jobsite.find(params[:id])
         @jobs = @jobsite.jobs
         @site_areas = @jobsite.site_areas
-        @site_areas << new_area
-        @site_areas.sort_by {|k, v| [v, k]}
-        @job_edit = params[:job]
-
+        new_area.name = params[:area][:name].capitalize
+        new_area.save
         erb :'jobs/edit'
     end
 
@@ -57,12 +48,26 @@ class JobController < ApplicationController
         @jobsite = Jobsite.find(params[:id])
         job = Job.create(params[:job])
         @jobsite.jobs << job
-        
+        areas = params[:areas]
+        areas.each do |a|
+            area = Area.find(a.to_i)
+            job.areas << area
+        end
+        job.save
         redirect "jobsite/#{@jobsite.id}/jobs/new"
     end
 
-    patch '/job/edit' do
-        
+    post '/jobsite/:id/jobs/edit/:job_id' do
+        job = Job.find(params[:job_id].to_i)
+        job.update(params[:job])
+        areas = params[:areas]
+            job.areas.clear
+            areas.each do |a|
+                area = Area.find(a.to_i)
+                job.areas << area
+            end
+        job.save
+        redirect "jobsite/#{params[:id]}"
     end
 
 
