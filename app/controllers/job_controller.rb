85e7234fabfb8bd11cb651c6d22982ef8f7e3ada
cassymarie <cassy2205@gmail.com>
2020-12-09 11:40:47 -0700

    Add Area to Job/Jobsite

--- app/controllers/job_controller.rb
+++ app/controllers/job_controller.rb
@@ -3,18 +3,52 @@ class JobController < ApplicationController
     get '/jobsite/:id/jobs/new' do
         @jobsite = Jobsite.find(params[:id])
         @jobs = @jobsite.jobs
-        @site_areas = @jobsite.site_areas
+        @site_areas = @jobsite.site_areas.sort_by {|k, v| [v, k]}
+        @job_edit = nil
         # binding.pry
         erb :'jobs/new'
     end
 
+    # get '/jobsite/:id/jobs/new/area' do
+    #     @jobsite = Jobsite.find(params[:id])
+    #     @jobs = @jobsite.jobs
+    #     @site_areas = @jobsite.site_areas
+    #     @job_edit = params[:job]
+    #     erb :'jobs/new'
+    # end
 
-    get '/jobsite/:id/jobs/show' do
+    get '/jobsite/:id/jobs/edit' do
         @jobsite = Jobsite.find(params[:id])
         @jobs = @jobsite.jobs
         @site_areas = @jobsite.site_areas
-        erb :'jobs/show'
+        @job_edit = nil
+        # binding.pry
+        erb :'jobs/edit'
     end
+
+    post '/jobsite/:id/jobs/new/area' do
+        new_area = Area.create(:code => params[:area][:code].upcase, :name => params[:area][:code].capitalize)
+        @jobsite = Jobsite.find(params[:id])
+        @jobs = @jobsite.jobs
+        @site_areas = @jobsite.site_areas
+        @site_areas << new_area
+        @site_areas.sort_by {|k, v| [v, k]}
+        @job_edit = params[:job]
+
+        binding.pry
+        erb :'jobs/new'
+    end
+
+    patch '/job/edit' do
+        
+    end
+
+    # get '/jobsite/:id/jobs/show' do
+    #     @jobsite = Jobsite.find(params[:id])
+    #     @jobs = @jobsite.jobs
+    #     @site_areas = @jobsite.site_areas
+    #     erb :'jobs/show'
+    # end
  
     post '/jobsite/:id/jobs/new' do
         @jobsite = Jobsite.find(params[:id])
@@ -30,4 +64,10 @@ class JobController < ApplicationController
         
     end
 
+    helpers do
+        def current_jobsite(id)
+          Jobsite.find(id)
+        end
+      end
+      
 end
--- app/models/area.rb
+++ app/models/area.rb
@@ -3,6 +3,5 @@ class Area < ActiveRecord::Base
     has_many :jobs, through: :job_areas
     
     validates :code, presence: true
-    validates :code, uniqueness: true
     validates :name, presence: true
 end
