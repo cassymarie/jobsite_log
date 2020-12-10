d0c9c4a1844cae62f40fc5609cac544e8c2d5d23
cassymarie <cassy2205@gmail.com>
2020-12-10 10:58:23 -0700

    layout for jobsite, created and applied to views

--- app/controllers/jobsite_controller.rb
+++ app/controllers/jobsite_controller.rb
@@ -5,7 +5,7 @@ class JobsiteController < ApplicationController
         user_sites = []
         UserJobsite.all.each {|site|user_sites << site.jobsite_id if site.user_id == @user.id}
         @jobsites = Jobsite.all.select {|site| user_sites.include?(site.id)}
-        erb :'jobsites/index'
+        erb :'jobsites/index', :layout => :'layouts/layout_jobsite'
     end
 
     get '/jobsite/:id' do 
@@ -14,7 +14,7 @@ class JobsiteController < ApplicationController
         @employees = @jobsite.employees
         @jobs = @jobsite.jobs
         @tasks = @jobsite.tasks
-        erb :'jobsites/show'
+        erb :'jobsites/show', :layout => :'layouts/layout_jobsite'
     end
 
     post '/jobsite/new' do
