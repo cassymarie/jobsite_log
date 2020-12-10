29259552f29a1cec63a1c84a93a9122b7f0bc828
cassymarie <cassy2205@gmail.com>
2020-12-10 12:45:38 -0700

    error message handling sign-in with invalid email

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -15,15 +15,21 @@ class SessionController < ApplicationController
     # (Invalid) sets the error message to be displayed on current page.
     post '/login' do
         @user = User.find_by(email: params[:user][":email"])
-        
+        @invalid = false
         #(Valid) User Email exists & password matches
         if @user && @user.authenticate(params[:user][":password"])
             session[:user_id] = @user.id
             redirect '/jobsites'
         else
-            @error = @user.errors.full_messages.join(" - ")
-            erb :'session/new'
+            if @user.nil?
+                @invalid = true
+                @error = "Email does not exist, Try again or Signup."
+            else
+                @error = @user.errors.full_messages.join(" - ")
+            end
+            erb :'session/new', :layout => :'layouts/layout_signin'
         end
+
     end
 
     get '/logout' do
