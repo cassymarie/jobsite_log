8a6b517b6c6df45530d2a7b61571fc2624ca21e6
cassymarie <cassy2205@gmail.com>
2020-12-10 16:13:38 -0700

    task edit/delete complete

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -8,7 +8,7 @@ class TaskController < ApplicationController
         @tasks = @jobsite.tasks
         erb :'tasks/add', :layout => :'layouts/layout_tasks'
     end
-    
+
     get '/jobsite/:id/task/edit/:task_id' do
         redirect '/error' if !logged_in?
         @jobsite = Jobsite.find(params[:id])
@@ -19,7 +19,7 @@ class TaskController < ApplicationController
         erb :'tasks/edit', :layout => :'layouts/layout_tasks'
     end
 
-    get '/jobsite/:id/tasks/delete/:task_id' do
+    get '/jobsite/:id/task/delete/:task_id' do
         redirect '/error' if !logged_in?
         @jobsite = Jobsite.find(params[:id])
         redirect '/jobsites' unless @jobsite.users.include?(current_user)
@@ -29,23 +29,32 @@ class TaskController < ApplicationController
         erb :'tasks/delete', :layout => :'layouts/layout_tasks'
     end 
 
+
+    patch '/task/edit/:task_id' do
+        jobsite = Jobsite.find(params[:jobsite_id])
+        task = Task.find_by(:id => params[:task_id])
+        task[:code] = params[:task][:code].upcase
+        task[:name] = params[:task][:name].capitalize
+        task.save
+        redirect "jobsite/#{jobsite.id}/tasks"
+    end
+
     post '/task/new' do
-        # binding.pry
         task = Task.create(params[:task])
         jobsite = Jobsite.find(params[:jobsite_id])
+        task.code.upcase!
+        task.name.capitalize!
         jobsite.tasks << task
         jobsite.save
         redirect "jobsite/#{jobsite.id}/tasks"
     end
 
-    patch '/tasks/edit' do
-        
-    end
 
-
-
-    delete '/tasks/delete/:id' do
-
+    delete '/task/delete/:task_id' do
+        jobsite = Jobsite.find(params[:jobsite_id])
+        task = Task.find(params[:task_id])
+        task.delete
+        redirect "jobsite/#{jobsite.id}/tasks"
     end
 
 end
