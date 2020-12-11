cf54317b91f990bcdaaaf2de61df27363fe4943f
cassymarie <cassy2205@gmail.com>
2020-12-11 09:23:08 -0700

    more styling seperated for jobsite/jobs

--- app/controllers/jobsite_controller.rb
+++ app/controllers/jobsite_controller.rb
@@ -3,10 +3,15 @@ class JobsiteController < ApplicationController
     get '/jobsites' do
         redirect '/error' if !logged_in? 
         @user = current_user
-        user_sites = []
-        UserJobsite.all.each {|site|user_sites << site.jobsite_id if site.user_id == @user.id}
-        @jobsites = Jobsite.all.select {|site| user_sites.include?(site.id)}
+        @jobsites = @user.jobsites
         erb :'jobsites/index', :layout => :'layouts/layout_jobsite'
+    end
+
+    get '/jobsites/new' do
+        redirect '/error' if !logged_in? 
+        @user = current_user
+        @jobsites = @user.jobsites
+        erb :'jobsites/new', :layout => :'layouts/layout_jobsite'
     end
 
     get '/jobsite/:id' do 
