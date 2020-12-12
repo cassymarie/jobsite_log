7289653b87a292f31a75414d56dfdd9ddf1ca924
cassymarie <cassy2205@gmail.com>
2020-12-12 07:39:16 -0700

    javascript setup for the the time_entry

---
+++ app/js/time_entry.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//  my page should total the hours by row 
+
+//  my page should have the grand total of hours
+
+//  Be able to add another job to any employee - maintaining hour calculations
+
